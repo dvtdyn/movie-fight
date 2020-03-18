fad26f32afa103d59f49951f34ff76d502839cb9
dvtdyn <dayan.davut@gmail.com>
2020-03-18 12:05:09 +0100

    Fetch on input

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hi there!')
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '41c26308',
+      s: searchTerm,
+    },
+  })
+
+  console.log(response.data)
+}
+
+const input = document.querySelector('input')
+
+const debounce = (func, delay = 500) => {
+  let timeoutID
+
+  return (...args) => {
+    if (timeoutID) {
+      clearTimeout(timeoutID)
+    }
+    timeoutID = setTimeout(() => func.apply(null, args), delay)
+  }
+}
+
+const onInput = event => {
+  fetchData(event.target.value)
+}
+
+input.addEventListener('input', debounce(onInput, 500))
