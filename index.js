f6aec66174746067f04dcee7496801ab319ee553
dvtdyn <dayan.davut@gmail.com>
2020-03-18 13:46:54 +0100

    Add autocomplete to input

--- index.js
+++ index.js
@@ -6,24 +6,56 @@ const fetchData = async searchTerm => {
     },
   })
 
-  console.log(response.data)
+  if (response.data.Error) {
+    return []
+  }
+
+  return response.data.Search
 }
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+<label>
+  <strong>Search For a Movie</strong>
+</label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results"></div>
+  </div>
+</div>
+`
+
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
-const debounce = (func, delay = 500) => {
-  let timeoutID
+const onInput = async event => {
+  const movies = await fetchData(event.target.value)
+  if (!movies.length) {
+    dropdown.classList.remove('is-active')
+    return
+  }
+  resultsWrapper.innerHTML = ''
+  dropdown.classList.add('is-active')
 
-  return (...args) => {
-    if (timeoutID) {
-      clearTimeout(timeoutID)
-    }
-    timeoutID = setTimeout(() => func.apply(null, args), delay)
+  for (let movie of movies) {
+    const option = document.createElement('a')
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+
+    option.classList.add('dropdown-item')
+    option.innerHTML = `
+    <img src="${imgSrc}"/>
+    ${movie.Title}
+    `
+    resultsWrapper.appendChild(option)
   }
 }
 
-const onInput = event => {
-  fetchData(event.target.value)
-}
-
 input.addEventListener('input', debounce(onInput, 500))
+
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active')
+  }
+})
